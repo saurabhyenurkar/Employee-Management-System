0bc4031bcbcf0141f1b161dbefeadc4e715bd2eb
Saurabh Yenurkar <117267956+saurabhyenurkar@users.noreply.github.com>
2023-08-05 22:04:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,7 @@ import multer from 'multer'
 import path from 'path'
 
 const app = express();
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://saurabhyenurkar.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+
 
 app.use(cors({
     origin: ["http://localhost:5173/"],
